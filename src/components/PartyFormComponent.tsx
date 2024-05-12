57ee0e2c2d7f15cc177f3af57f42b3da44c52d7c
Cass <dr.shaid@gmail.com>
2024-05-12 22:56:27 +1000

    Add movie images and component

--- src/components/PartyFormComponent.tsx
+++ src/components/PartyFormComponent.tsx
@@ -1,18 +1,32 @@
-import getBlob from "../utils/getBlob"
-import setBlob from "../utils/setBlob"
+import type { Props } from "astro"
+//import getBlob from "../utils/getBlob"
+//import setBlob from "../utils/setBlob"
 
 const processForm = (e: Event) => {
     const input: EventTarget | null = e.target
     console.log('Form!', input.name, input.value)
 }
 
-export default () =>
+const pickMovie = (e: Event) => {
+    const movie: EventTarget = e.target
+    console.log('Movie!', movie.id)
+
+}
+
+
+export default (props: Props) =>
     {
+        console.log(props)
         return (
             <form name='PartyForm' >
                 <div>If you have any food allergies please write them below</div>
                 <input type="text" name="test" onBlur={processForm} />
                 <h4>Voting for the movie</h4>
+
+                {props.children}
             </form>
+
         )
     }
+
+   
