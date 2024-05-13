f8e212923f4696266e7b349eea74ac9f585592ae
Cass <dr.shaid@gmail.com>
2024-05-13 22:35:22 +1000

    Final style and content updates.

--- src/components/PartyFormComponent.tsx
+++ src/components/PartyFormComponent.tsx
@@ -6,17 +6,18 @@ import debounce from 'debounce'
 
 const baseUrl = 'https://katarinas-8th.paperkat.art'
 
-const processForm = (e: Event, props: Props) => {
+const processForm = async (e: Event, props: Props) => {
     const input: EventTarget | null = e.target
     selections.food = input.value
-    console.log('Form!', selections)
+
+    await pushState(props.who)
 }
 
 const pickMovie = async (e: Event, props: Props) => {
     const movie: EventTarget = e.target
     if(movie.type == "radio") {
-        console.log('Movie!', movie.value)
         selections.movie = movie.value
+
         await pushState(props.who)
     }
 
@@ -31,7 +32,6 @@ const selections = signal({
 const getState = async (who: string) => {
     const data = await fetch(`${baseUrl}/api/get/${who}`)
     const newState = await data.json()
-    console.log(moviesRef, newState)
 
     if(typeof window !== 'undefined') {
         window.document.getElementById(`movie_${newState.movie}`).checked = true
@@ -43,7 +43,6 @@ const getState = async (who: string) => {
 
 
 const pushState = async (who: string) => {
-    console.log('BLOB PUSH')
     await fetch(`${baseUrl}/api/set/${who}`, {
         method: 'POST',
         body: JSON.stringify({
@@ -57,24 +56,35 @@ const pushState = async (who: string) => {
 const moviesRef = createRef();
 
 export default (props: Props) =>
-    {
-        getState(props.who)
+{
+    getState(props.who)
 
-        if(props.who == 'cassandra') {
-            return (
-                <form name='PartyForm' >
-                    <div>If you have any food allergies please write them below</div>
-                    <input type="text" id="text_allergies" name="allergies" onChange={e => processForm(e, props)} value={selections.food}/>
-                    <fieldset ref={moviesRef} onClick={debounce(e => pickMovie(e, props), 1000)}>
-                        <legend>
-                            <h4>Voting for the movie</h4>
-                        </legend>
-                        {props.children}
-                    </fieldset>
-                </form>
+    return (
+        <form name='PartyForm' >
+            <h4>If you have any food allergies please enter them below:</h4>
+            <input 
+                type="text" 
+                id="text_allergies" 
+                name="allergies" 
+                onChange={e => processForm(e, props)} value={selections.food}
+                style="font-size: 1rem; color: #c3cadb; background-color: #ffffff00; border: 0px; border-bottom: 3px solid purple; margin: 1rem 0rem; padding: 1rem;"
+                default="Please enter any food allergies."
+            />
+        
+            <h4 style="margin: 1rem 0;">Voting for the movie!</h4>
+            <div>Please select the movie you'd most like to watch.</div>
+            <fieldset 
+                ref={moviesRef} 
+                onClick={debounce(e => pickMovie(e, props), 1000)}
+                style="border: 1px solid #283044; margin: 2rem 0; padding: 2rem; border-radius: 1rem;"
+            >
+                <legend>
+                </legend>
+                {props.children}
+            </fieldset>
+        </form>
 
-            )
-        }
-    }
+    )
+}
 
    
