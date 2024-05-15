9219cd8d2151959cdcf45811782210a4df0f1efc
Cass <dr.shaid@gmail.com>
2024-05-15 20:46:45 +1000

    Debug round 1

--- netlify/functions/getVotes.ts
+++ netlify/functions/getVotes.ts
@@ -7,9 +7,9 @@ export default async (request: Request, context: Context) => {
     const { who } = context.params
     const store = getStore({
       name: "katarinas-8th",
-      consistency: "strong" 
     })
     const { data } = await store.list()
+    console.log(data)
   
     return new Response(data, defaultFetchOptions)
 }
