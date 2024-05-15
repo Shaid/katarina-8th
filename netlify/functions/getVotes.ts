cbf6df5e0b08d1cab79fa9fb95f277a2d6d1655f
Cass <dr.shaid@gmail.com>
2024-05-15 20:39:39 +1000

    Add list api endpoint.

---
+++ netlify/functions/getVotes.ts
@@ -0,0 +1,19 @@
+import type { Config, Context } from "@netlify/functions"
+import { getStore } from "@netlify/blobs"
+import { defaultFetchOptions } from './defaults.ts'
+
+
+export default async (request: Request, context: Context) => {
+    const { who } = context.params
+    const store = getStore({
+      name: "katarinas-8th",
+      consistency: "strong" 
+    })
+    const data = await store.list(who)
+  
+    return new Response(data, defaultFetchOptions)
+}
+
+export const config: Config = {
+  path: "/api/votes/"
+}
