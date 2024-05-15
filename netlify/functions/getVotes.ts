e665da68809d39f885d608c9fa62eff8e6f8632e
Cass <dr.shaid@gmail.com>
2024-05-15 23:20:07 +1000

    at some point this is just stream of conciousness

--- netlify/functions/getVotes.ts
+++ netlify/functions/getVotes.ts
@@ -18,7 +18,7 @@ export default async (request: Request, context: Context) => {
 
     for (var invitee of invitees) {
         const vote = await store.get(invitee, { type: 'json'})
-        console.log(invitee, vote, typeof vote, typeof vote.movie)
+        console.log(invitee, vote)
         if(typeof vote !== null && typeof vote.movie !== undefined) {
 		    votes[vote.movie]++
         }
